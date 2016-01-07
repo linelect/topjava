81d036def1ab3a01feec640439bc174fe3919b1d
linelect <pavlo.dudch@gmail.com>
2016-01-07 22:20:57 +0200

    fix MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -31,6 +31,7 @@ public class MealServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        springContext.getEnvironment().setActiveProfiles("postgres, datajpa");
         springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
         mealController = springContext.getBean(UserMealRestController.class);
     }

59158778e2086d3022d324a87d585f355d230eba
linelect <pavlo.dudch@gmail.com>
2016-01-07 16:56:25 +0200

    HW5 first task

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/ProxyUserMealRepository.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model.UserMeal;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * GKislin
+ * 27.03.2015.
+ */
+@Transactional(readOnly = true)
+public interface ProxyUserMealRepository extends JpaRepository<UserMeal, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(name = UserMeal.DELETE)
+//    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Override
+    @Transactional
+    UserMeal save(UserMeal userMeal);
+
+    @Query(name = UserMeal.GET)
+    UserMeal findOne(@Param("id") int id, @Param("userId") int userId);
+
+    @Query(name = UserMeal.ALL_SORTED)
+    List<UserMeal> findAll(@Param("userId") int userId);
+
+    @Query(name = UserMeal.GET_BETWEEN)
+    List<UserMeal> getBetween(@Param("userId") int userId, @Param("startDate")LocalDateTime startDate, @Param("endDate")LocalDateTime endDate);
+
+}

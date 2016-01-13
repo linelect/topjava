27d87431e1ff83c5cb6ae62688ee09b8f529737a
linelect <pavlo.dudch@gmail.com>
2016-01-13 23:07:15 +0200

    fix jdbcImp and tests

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -1,18 +1,26 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 /**
  * User: gkislin
@@ -57,7 +65,21 @@ public class JdbcUserRepositoryImpl implements UserRepository {
             namedParameterJdbcTemplate.update(
                     "UPDATE users SET name=:name, email=:email, password=:password, " +
                             "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", map);
+
+            MapSqlParameterSource mapRole = new MapSqlParameterSource()
+                    .addValue("user_id", user.getId());
+
+            jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", user.getId());
         }
+
+        for (Role r : user.getRoles()) {
+            MapSqlParameterSource mapRole = new MapSqlParameterSource()
+                    .addValue("user_id", user.getId())
+                    .addValue("role", r.name());
+            namedParameterJdbcTemplate.update(
+                    "INSERT INTO user_roles (role, user_id) VALUES (:role, :user_id)", mapRole);
+        }
+
         return user;
     }
 
@@ -79,6 +101,41 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+
+        List<User> userList = jdbcTemplate.query("SELECT DISTINCT * FROM users u left outer join user_roles r on u.id=r.user_id ORDER BY name, email, role Desc", new ResultSetExtractor<List<User>>() {
+            @Override
+            public List<User> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+                Map<Integer, User> usersMap = new HashMap<Integer, User>();
+                while(resultSet.next()) {
+                    Integer userId = resultSet.getInt("id");
+                    User user = usersMap.get(userId);
+                    if (user == null) {
+                        user = ROW_MAPPER.mapRow(resultSet, resultSet.getRow());
+                        usersMap.put(userId,user);
+                    }
+
+                    String roleName = resultSet.getString("role");
+                    if (roleName != null) {
+                        Role role = Role.valueOf(roleName);
+                        if (user.getRoles() == null) {
+                            user.setRoles(new HashSet<>());
+                        }
+                        user.addRole(role);
+                    }
+                }
+                return new ArrayList<User>(usersMap.values());
+            }
+        });
+
+        Collections.sort(userList, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int nameVal = o1.getName().compareTo(o2.getName()) * 10;
+                int emailval = o1.getEmail().compareTo(o2.getEmail());
+                return nameVal + emailval;
+            }
+        });
+
+        return userList;
     }
 }

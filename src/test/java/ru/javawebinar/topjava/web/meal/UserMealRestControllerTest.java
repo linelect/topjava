5a9f9565ef9960f81f1f98664f254a4f3de4ee96
linelect <pavlo.dudch@gmail.com>
2016-01-19 18:50:11 +0200

    add meal rest test

--- src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
@@ -1,35 +1,60 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.service.UserMealService;
+import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.user.AdminRestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static ru.javawebinar.topjava.MealTestData.USER_MEALS;
 import static ru.javawebinar.topjava.MealTestData.MEAL1;
-
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.MealTestData.MATCHER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 
 public class UserMealRestControllerTest extends AbstractControllerTest {
 
+//    public static final String REST_URL = UserMealRestController.REST_URL + '/';
+    public static final String REST_URL = "/rest/meal/";
+
+    @Autowired
+    protected UserMealService userMealService;
+
     @Test
-    public void testUserMealList() throws Exception {
-        mockMvc.perform(get("/meals"))
-                .andDo(print())
+    public void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
                 .andExpect(status().isOk())
-                .andExpect(view().name("mealList"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/mealList.jsp"))
-                .andExpect(model().attribute("mealList", hasSize(USER_MEALS.size())))
-                .andExpect(model().attribute("mealList", hasItem(
-                        allOf(
-                                hasProperty("dateTime", is(MEAL1.getDateTime())),
-                                hasProperty("description", is(MEAL1.getDescription()))
-                        )
-                )));
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentMatcher(MEAL1));
     }
 
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL + MEAL1_ID).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+        ArrayList<UserMeal> mealsAfterDelete = new ArrayList<>(USER_MEALS);
+        mealsAfterDelete.remove(MEAL1);
+        MATCHER.assertCollectionEquals(mealsAfterDelete, userMealService.getAll(MEAL1.getUser().getId()));
+    }
 }
